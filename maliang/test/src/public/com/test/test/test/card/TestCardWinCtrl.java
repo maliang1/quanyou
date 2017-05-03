5b34a509c289364aec7800806e5e9cdc156db6a8
maliang <809330025@qq.com>
2017-05-03 09:06:07 +0800

    删除项目

--- maliang/test/src/public/com/test/test/test/card/TestCardWinCtrl.java
+++
@@ -1,5 +0,0 @@
-package com.test.test.test.card;
-import java.io.Serializable;
-public class TestCardWinCtrl implements Serializable {
-  private static final long serialVersionUID=7532916478964732880L;
-}
--- maliang/test/web/html/nodes/com/test/test/uiComps/component.cp
+++
@@ -1,2 +0,0 @@
-<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<Component id="uiComps" name="uiComps" pack="com.test.test"/>
